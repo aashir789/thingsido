a7c80798bbb8acfea205aaa8a1c3f7c6be953585
emaashir789/em <aashir789@gmail.com>
2015-06-25 16:45:15 -0400

    animations with add new post

---
+++ animations.js
@@ -0,0 +1,12 @@
+function showInput(){
+    $("#input").show();
+    $("#addPost").hide();
+}
+
+function cancelNewPost(){
+    // also add a feature to reset the input value
+    $("#input").hide();
+    $("#addPost").show();
+
+}
+
